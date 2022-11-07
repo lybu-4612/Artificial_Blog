075fd61738c5dcd9874eba163e8d7f0cebaa70a0
lybu-4612 <lybu4612@gmail.com>
2022-11-07 17:03:13 +0530

    first commit

---
+++ blog.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Lybu S S',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Arjun ',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+    
+    
+@app.route("/about")
+def about():
+    return render_template('about.html', title="About")
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
