b8f65b9575be111f53ee36909db66af23dcadbf0
lybu-4612 <lybu4612@gmail.com>
2022-12-09 17:46:03 +0530

    Account Page commit

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
 from blog.models import User
@@ -22,6 +23,30 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError(' That email is taken , Choose a new username')
+
+    
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                        validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                    validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Update')
+
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(' That Username is taken , Choose a new username')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(' That email is taken , Choose a new username')
 
 
 class LoginForm(FlaskForm):
